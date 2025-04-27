bc35f8721136bf448231e4d5ea4f18e87fd1c115
Sergio Rebollo <sergio.rebollo@tersoalit.com>
2025-04-27 20:43:19 +0200

    Add create structure script and template of the day class.

---
+++ create_structure.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App;
+
+require_once 'autoload.php';
+
+if ($argc != 3) {
+    echo "Execute command as: php app.php <year> <day>\n";
+    exit(1);
+}
+
+$year = (int)$argv[1];
+$day = (int)$argv[2];
+
+if ($day < 0 || $day > 24) {
+    echo "Day must be 1 to 24, or 0 to create all days\n";
+    exit(1);
+}
+
+createDataStructure($year, $day);
+createClassStructure($year, $day);
+
+function createDataStructure(int $year, int $day): void
+{
+    $yearDataFolderPath = __DIR__ . "/data/$year";
+    if (!is_dir($yearDataFolderPath)) {
+        mkdir($yearDataFolderPath);
+    }
+
+    $days = [$day];
+    if ($day === 0) {
+        $days = range(1, 24);
+    }
+
+    foreach ($days as $day) {
+        $dayName = strlen($day) === 1 ? "0" . $day : (string)$day;
+        $dayDataFolderPath = $yearDataFolderPath . "/day$dayName";
+
+        if (!is_dir($dayDataFolderPath)) {
+            mkdir($dayDataFolderPath);
+        }
+
+        $testFilePath = $dayDataFolderPath . "/day$dayName-test.txt";
+        if (!is_file($testFilePath)) {
+            touch($testFilePath);
+        }
+
+        $filePath = $dayDataFolderPath . "/day$dayName.txt";
+        if (!is_file($filePath)) {
+            touch($filePath);
+        }
+    }
+}
+
+function createClassStructure(int $year, int $day): void
+{
+    $yearFolderPath = __DIR__ . "/src/Y$year";
+    if (!is_dir($yearFolderPath)) {
+        mkdir($yearFolderPath);
+    }
+
+    $days = [$day];
+    if ($day === 0) {
+        $days = range(1, 24);
+    }
+
+    $classTplFilePath = __DIR__ . "/DayXX.tpl";
+    $classTplContent = file_get_contents($classTplFilePath);
+
+    foreach ($days as $day) {
+        $dayName = strlen($day) === 1 ? "0" . $day : (string)$day;
+        $classFilePath = $yearFolderPath . "/Day$dayName.php";
+
+        if (is_file($classFilePath)) {
+            continue;
+        }
+
+        $classFileContent = $classTplContent;
+        $classFileContent = str_replace("class DayXX extends DayBase", "class Day$dayName extends DayBase", $classFileContent);
+        $classFileContent = str_replace("namespace App\YXXXX;", "namespace App\Y$year;", $classFileContent);
+
+        file_put_contents($classFilePath, $classFileContent);
+    }
+}
+
+
