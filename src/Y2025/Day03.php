2e2e14cfa490fd53b3dba61d95a5259af0c47cca
Sergio Rebollo <sergio.rebollo@tersoalit.com>
2025-12-05 21:49:12 +0100

    Add 2025 day 3.

--- src/Y2025/Day03.php
+++ src/Y2025/Day03.php
@@ -6,12 +6,16 @@ use App\Model\DayBase;
 
 class Day03 extends DayBase
 {
-    protected const int TEST_1 = 0;
-    protected const int TEST_2 = 0;
+    protected const int TEST_1 = 357;
+    protected const int TEST_2 = 3121910778619;
 
     public function loadData(string $filePath): void
     {
         $this->loadDataAsArray($filePath, "\n");
+
+        foreach ($this->data as $key => $row) {
+            $this->data[$key] = str_split($row);
+        }
 
 //        print_r($this->data);
 //        echo PHP_EOL;
@@ -27,11 +31,69 @@ class Day03 extends DayBase
 
     private function getPart1(): int
     {
-        return 0;
+        if ($this->test) {
+            echo '=========================' . PHP_EOL;
+            echo 'Test 1' . PHP_EOL;
+            echo '=========================' . PHP_EOL;
+        }
+
+        return array_sum($this->getMaximumJoltages(2));
     }
 
     private function getPart2(): int
     {
-        return 0;
+        if ($this->test) {
+            echo '=========================' . PHP_EOL;
+            echo 'Test 2' . PHP_EOL;
+            echo '=========================' . PHP_EOL;
+        }
+
+        return array_sum($this->getMaximumJoltages(12));
+    }
+
+    private function getMaximumJoltages(int $numberOfDigits): array
+    {
+        $joltages = [];
+
+        foreach ($this->data as $digits) {
+            $joltages[] = $this->findLargestJoltageNumber($digits, $numberOfDigits);
+        }
+
+        if ($this->test) {
+            print_r($joltages);
+        }
+
+        return $joltages;
+    }
+
+    private function findLargestJoltageNumber(array $digits, int $numberOfDigits): int
+    {
+        if ($this->test) {
+            echo '-----------------------------------------' . PHP_EOL;
+            echo 'Digits: ' . implode('', $digits) . PHP_EOL;
+            echo '-----------------------------------------' . PHP_EOL;
+        }
+
+        $numbers = [];
+        $init = 0;
+
+        for ($i = $numberOfDigits; $i > 0; $i--) {
+            $digitArrange = array_slice($digits, $init, count($digits) - $init - $i + 1, true);
+
+            if ($this->test) {
+                echo 'Digits arrange slice init: ' . $init . PHP_EOL;
+                echo 'Digits arrange slice length: ' . (count($digits) - $init - $i + 1) . PHP_EOL;
+                echo 'Digits arrange: ' . implode('', $digitArrange) . PHP_EOL;
+            }
+
+            arsort($digitArrange);
+
+            $init = array_key_first($digitArrange);
+            $numbers[] = $digitArrange[$init];
+
+            $init++;
+        }
+
+        return (int) implode('', $numbers);
     }
 }
