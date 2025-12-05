be0f108777d6a522fb2898a797bcc568f208e83c
Sergio Rebollo <sergio.rebollo@tersoalit.com>
2025-12-05 18:08:24 +0100

    Add 2025 day 1.

--- src/Y2025/Day01.php
+++ src/Y2025/Day01.php
@@ -6,8 +6,12 @@ use App\Model\DayBase;
 
 class Day01 extends DayBase
 {
-    protected const int TEST_1 = 0;
-    protected const int TEST_2 = 0;
+    protected const int TEST_1 = 3;
+    protected const int TEST_2 = 6;
+
+    protected const int INITIAL_POINT = 50;
+    protected const string LEFT = 'L';
+    protected const string RIGHT = 'R';
 
     public function loadData(string $filePath): void
     {
@@ -27,11 +31,71 @@ class Day01 extends DayBase
 
     private function getPart1(): int
     {
-        return 0;
+        if ($this->test) {
+            echo '=========================' . PHP_EOL;
+            echo 'Test 1' . PHP_EOL;
+            echo '=========================' . PHP_EOL;
+        }
+
+        return $this->getPassword(false);
     }
 
     private function getPart2(): int
     {
-        return 0;
+        if ($this->test) {
+            echo '=========================' . PHP_EOL;
+            echo 'Test 2' . PHP_EOL;
+            echo '=========================' . PHP_EOL;
+        }
+
+        return $this->getPassword(true);
+    }
+
+    private function getPassword(bool $returnClicks = false): int
+    {
+        $point = self::INITIAL_POINT;
+        $resultPoint = 0;
+        $resultClicks = 0;
+
+        foreach ($this->data as $value) {
+            $direction = substr($value, 0, 1);
+            $distance = (int)substr($value, 1);
+
+            [$point, $clicks] = $this->turnDial($point, $direction, $distance);
+
+            if ($point === 0) {
+                $resultPoint++;
+            }
+
+            $resultClicks += $clicks;
+
+            if ($this->test) {
+                echo 'The dial is rotated ' . $direction . $distance . ' to point at ' . $point . ' and has ' . $clicks . ' clicks' . PHP_EOL;
+            }
+        }
+
+        return $returnClicks ? $resultClicks : $resultPoint;
+    }
+
+    private function turnDial(int $currentPoint, string $direction, int $distance): array
+    {
+        $dirMultiplier = $direction === self::LEFT ? -1 : 1;
+        $point = $currentPoint + ($distance * $dirMultiplier);
+
+        $newPoint = $point % 100;
+
+        if ($newPoint < 0) {
+            $newPoint += 100;
+        }
+
+        $clicks = (int) abs($point / 100);
+
+        if ($clicks === 0 && $point <= 0 && $currentPoint !== 0) {
+            $clicks++;
+        } elseif ($dirMultiplier < 0 && $point < 0 && $currentPoint !== 0) {
+            $clicks++;
+        }
+
+        return [$newPoint, $clicks];
     }
 }
