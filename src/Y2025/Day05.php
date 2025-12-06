a34eb168a5ce7353d7b6f6e412658aa0583221c1
Sergio Rebollo <sergio.rebollo@tersoalit.com>
2025-12-06 12:13:10 +0100

    Add 2025 day 5.

--- src/Y2025/Day05.php
+++ src/Y2025/Day05.php
@@ -6,14 +6,31 @@ use App\Model\DayBase;
 
 class Day05 extends DayBase
 {
-    protected const int TEST_1 = 0;
-    protected const int TEST_2 = 0;
+    protected const int TEST_1 = 3;
+    protected const int TEST_2 = 14;
+
+    protected array $freshIngredientIdRanges = [];
+    protected array $ingredientIds = [];
 
     public function loadData(string $filePath): void
     {
         $this->loadDataAsArray($filePath, "\n");
 
-//        print_r($this->data);
+        foreach ($this->data as $line) {
+            if (empty($line)) {
+                continue;
+            }
+
+            if (str_contains($line, '-')) {
+                $range = explode('-', $line);
+                $this->freshIngredientIdRanges[] = ['from'  => (int)$range[0], 'to' => (int)$range[1]];
+            } else {
+                $this->ingredientIds[] = (int)$line;
+            }
+        }
+
+//        print_r($this->freshIngredientIdRanges);
+//        print_r($this->ingredientIds);
 //        echo PHP_EOL;
     }
 
@@ -27,11 +44,98 @@ class Day05 extends DayBase
 
     private function getPart1(): int
     {
-        return 0;
+        $fresh = [];
+
+        foreach ($this->ingredientIds as $ingredientId) {
+            foreach ($this->freshIngredientIdRanges as $range) {
+                if ($ingredientId < $range['from']) {
+                    continue;
+                }
+
+                if ($ingredientId > $range['to']) {
+                    continue;
+                }
+
+                $fresh[] = $ingredientId;
+                break;
+            }
+        }
+
+        return count($fresh);
     }
 
     private function getPart2(): int
     {
-        return 0;
+        $uniqueFreshRanges = [];
+
+        foreach ($this->freshIngredientIdRanges as $index => $range) {
+            $from = $range['from'];
+            $to = $range['to'];
+
+            if (empty($uniqueFreshRanges)) {
+                $uniqueFreshRanges[] = ['from' => $from, 'to' => $to];
+
+                continue;
+            }
+
+            $this->getUniqueFreshRanges($index, $from, $to, $uniqueFreshRanges);
+        }
+
+        print_r($uniqueFreshRanges);
+
+        $freshCount = 0;
+
+        foreach ($uniqueFreshRanges as $uniqueFreshRange) {
+            $freshCount += $uniqueFreshRange['to'] - $uniqueFreshRange['from'] + 1;
+        }
+
+        return $freshCount;
+    }
+
+    private function getUniqueFreshRanges(int $index, int $from, int $to, array &$uniqueFreshRanges): void
+    {
+        foreach ($uniqueFreshRanges as $uniqueFreshRange) {
+            if ($from >= $uniqueFreshRange['from'] && $from <= $uniqueFreshRange['to'] && $to <= $uniqueFreshRange['to']) {
+                return;
+            }
+
+            if ($from < $uniqueFreshRange['from'] && $to > $uniqueFreshRange['to']) {
+
+                echo 'Range ' . $from . ' - ' . $to . ' at line ' . $index + 1 . ' is wider than ' . $uniqueFreshRange['from'] . ' - ' . $uniqueFreshRange['to'] . PHP_EOL;
+
+                $this->getUniqueFreshRanges($index, $from, $uniqueFreshRange['from'] - 1, $uniqueFreshRanges);
+                $this->getUniqueFreshRanges($index, $uniqueFreshRange['to'] + 1, $to, $uniqueFreshRanges);
+
+                return;
+            }
+
+            if ($from > $uniqueFreshRange['to']) {
+                continue;
+            }
+
+            if ($from < $uniqueFreshRange['from'] && $to < $uniqueFreshRange['from']) {
+                continue;
+            }
+
+            if ($from < $uniqueFreshRange['from'] && $to <= $uniqueFreshRange['to']) {
+
+                echo 'Range ' . $from . ' - ' . $to . ' at line ' . $index + 1 . ' overlaps ' . $uniqueFreshRange['from'] . ' - ' . $uniqueFreshRange['to'] . PHP_EOL;
+
+                $this->getUniqueFreshRanges($index, $from, $uniqueFreshRange['from'] - 1, $uniqueFreshRanges);
+
+                return;
+            }
+
+            if ($from >= $uniqueFreshRange['from'] && $from <= $uniqueFreshRange['to'] && $to > $uniqueFreshRange['to']) {
+
+                echo 'Range ' . $from . ' - ' . $to . ' at line ' . $index + 1 . ' overlaps ' . $uniqueFreshRange['from'] . ' - ' . $uniqueFreshRange['to'] . PHP_EOL;
+
+                $this->getUniqueFreshRanges($index, $uniqueFreshRange['to'] + 1, $to, $uniqueFreshRanges);
+
+                return;
+            }
+        }
+
+        $uniqueFreshRanges[] = ['from' => $from, 'to' => $to];
     }
 }
