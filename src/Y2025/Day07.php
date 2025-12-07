834f07e34b6e0ce80c33086d0e89ebb7eb4c05a9
Sergio Rebollo <sergio.rebollo@tersoalit.com>
2025-12-07 22:09:22 +0100

    Add 2025 day 7.

--- src/Y2025/Day07.php
+++ src/Y2025/Day07.php
@@ -6,15 +6,15 @@ use App\Model\DayBase;
 
 class Day07 extends DayBase
 {
-    protected const int TEST_1 = 0;
-    protected const int TEST_2 = 0;
+    protected const int TEST_1 = 21;
+    protected const int TEST_2 = 40;
 
     public function loadData(string $filePath): void
     {
-        $this->loadDataAsArray($filePath, "\n");
+        $this->loadDataAsArrayMap($filePath, "\n");
 
-//        print_r($this->data);
-//        echo PHP_EOL;
+        $this->printMap($this->data);
+        echo PHP_EOL;
     }
 
     public function getResult(): array
@@ -27,11 +27,113 @@ class Day07 extends DayBase
 
     private function getPart1(): int
     {
-        return 0;
+        [$splitCounter, $timelinesCounter] = $this->getBeamsSplitData(false);
+
+        return $splitCounter;
     }
 
     private function getPart2(): int
     {
-        return 0;
+        [$splitCounter, $timelinesCounter] = $this->getBeamsSplitData(true);
+
+        return $timelinesCounter;
+    }
+
+    private function getBeamsSplitData(bool $sumAllInRow): array
+    {
+        $initialPosition = $this->getInitialPosition($this->data);
+        $initialPositionKey = $initialPosition[0] . '-' . $initialPosition[1];
+
+        $beams[$initialPositionKey] = 1;
+        $splitCounter = 0;
+
+        foreach ($this->data as $row => $columns) {
+            $beams = $this->getBeamsInRow($row, $beams, $sumAllInRow, $splitCounter);
+
+            if ($this->test) {
+                echo 'Beams in row ' . $row . ': ' . implode(',', array_keys($beams)) . PHP_EOL;
+
+                $this->printNewMap($this->data, $beams);
+            }
+        }
+
+        return [$splitCounter, array_sum($beams)];
+    }
+
+    private function getBeamsInRow(int $row, array $beams, bool $sumAllInRow, int &$splitCounter): array
+    {
+        if ($row === 0) {
+            return $beams;
+        }
+
+        if (!array_key_exists($row, $this->data)) {
+            return $beams;
+        }
+
+        $newBeams = [];
+
+        foreach ($beams as $beam => $counter) {
+            [$y, $x] = explode('-', $beam);
+            $x = intval($x);
+
+            if ($this->data[$row][$x] !== self::DIRECTION_ARROW_TOP) {
+                $newPositionKey = $row . '-' . $x;
+
+                if (array_key_exists($newPositionKey, $newBeams)) {
+                    $newBeams[$newPositionKey] += $counter;
+                } else {
+                    $newBeams[$newPositionKey] = $counter;
+                }
+
+                continue;
+            }
+
+            $splitCounter++;
+
+            $newPositionKey = $row . '-' . ($x + 1);
+
+            if (array_key_exists($newPositionKey, $newBeams)) {
+                $newBeams[$newPositionKey] += $counter;
+            } else {
+                $newBeams[$newPositionKey] = $counter;
+            }
+
+            if ($sumAllInRow || !isset($this->data[$row][$x - 2]) || $this->data[$row][$x - 2] !== self::DIRECTION_ARROW_TOP) {
+                $newPositionKey = $row . '-' . ($x - 1);
+
+                if (array_key_exists($newPositionKey, $newBeams)) {
+                    $newBeams[$newPositionKey] += $counter;
+                } else {
+                    $newBeams[$newPositionKey] = $counter;
+                }
+            }
+        }
+
+        return $newBeams;
+    }
+
+    private function getInitialPosition(array $data): array
+    {
+        for ($y = 0; $y < count($data); $y++) {
+            for ($x = 0; $x < count($data[$y]); $x++) {
+                if ($data[$y][$x] !== self::START_POSITION) {
+                    continue;
+                }
+
+                return [$y, $x];
+            }
+        }
+
+        throw new \Exception("Invalid position");
+    }
+
+    private function printNewMap(array $map, array $beams): void
+    {
+        foreach ($beams as $beam => $counter) {
+            [$y, $x] = explode('-', $beam);
+            $map[$y][$x] = $counter;
+        }
+
+        $this->printMap($map);
     }
 }
