f4c8c434070b0446cacd7b066fe83351ee7427f6
Sergio Rebollo <sergio.rebollo@tersoalit.com>
2025-12-31 16:35:00 +0100

    Add 2022 day 1.

--- app/src/Y2022/Day01.php
+++ app/src/Y2022/Day01.php
@@ -6,15 +6,30 @@ use App\Model\DayBase;
 
 class Day01 extends DayBase
 {
-    protected const int TEST_1 = 0;
-    protected const int TEST_2 = 0;
+    protected const int TEST_1 = 24000;
+    protected const int TEST_2 = 45000;
+
+    private array $calories = [];
 
     public function loadData(string $filePath): void
     {
         $this->loadDataAsArray($filePath, "\n");
 
+        $index = 0;
+
+        foreach ($this->data as $line) {
+            if (empty($line)) {
+                $index++;
+
+                continue;
+            }
+
+            $this->calories[$index][] = (int)$line;
+        }
+
         if ($this->test) {
             print_r($this->data);
+            print_r($this->calories);
             echo PHP_EOL;
         }
     }
@@ -29,11 +44,20 @@ class Day01 extends DayBase
 
     private function getPart1(): int
     {
-        return 0;
+        return $this->getTotalCalories(1);
     }
 
     private function getPart2(): int
     {
-        return 0;
+        return $this->getTotalCalories(3);
+    }
+
+    private function getTotalCalories(int $mostCaloriesCount): int
+    {
+        $totalCalories = array_map(fn($calories) => array_sum($calories), $this->calories);
+
+        sort($totalCalories);
+
+        return array_sum(array_slice($totalCalories, -$mostCaloriesCount));
     }
 }
