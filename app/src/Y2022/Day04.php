3ec73d76c784d8699712d2befeb1d35177f8d385
Sergio Rebollo <sergio.rebollo@tersoalit.com>
2025-12-31 18:13:12 +0100

    Add 2022 day 4.

--- app/src/Y2022/Day04.php
+++ app/src/Y2022/Day04.php
@@ -6,12 +6,17 @@ use App\Model\DayBase;
 
 class Day04 extends DayBase
 {
-    protected const int TEST_1 = 0;
-    protected const int TEST_2 = 0;
+    protected const int TEST_1 = 2;
+    protected const int TEST_2 = 4;
 
     public function loadData(string $filePath): void
     {
         $this->loadDataAsArray($filePath, "\n");
+
+        $this->data = array_map(function ($line) {
+            $pairs = explode(',', $line);
+            return array_map(fn ($pair) => explode('-', $pair), $pairs);
+        }, $this->data);
 
         if ($this->test) {
             print_r($this->data);
@@ -29,11 +34,33 @@ class Day04 extends DayBase
 
     private function getPart1(): int
     {
-        return 0;
+        $containingPairsCount = 0;
+
+        foreach ($this->data as $pairs) {
+            $pair1 = range($pairs[0][0], $pairs[0][1]);
+            $pair2 = range($pairs[1][0], $pairs[1][1]);
+
+            if (array_all($pair1, fn ($value) => in_array($value, $pair2)) || array_all($pair2, fn ($value) => in_array($value, $pair1))) {
+                $containingPairsCount++;
+            }
+        }
+
+        return $containingPairsCount;
     }
 
     private function getPart2(): int
     {
-        return 0;
+        $overlapPairsCount = 0;
+
+        foreach ($this->data as $pairs) {
+            $pair1 = range($pairs[0][0], $pairs[0][1]);
+            $pair2 = range($pairs[1][0], $pairs[1][1]);
+
+            if (array_intersect($pair1, $pair2)) {
+                $overlapPairsCount++;
+            }
+        }
+
+        return $overlapPairsCount;
     }
 }
