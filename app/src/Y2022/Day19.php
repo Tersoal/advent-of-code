a681a9ce6cd8d2b3e712274e64480f48210c99e9
Sergio Rebollo <sergio.rebollo@tersoalit.com>
2025-12-31 16:24:49 +0100

    Add 2022 base.

---
+++ app/src/Y2022/Day19.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Y2022;
+
+use App\Model\DayBase;
+
+class Day19 extends DayBase
+{
+    protected const int TEST_1 = 0;
+    protected const int TEST_2 = 0;
+
+    public function loadData(string $filePath): void
+    {
+        $this->loadDataAsArray($filePath, "\n");
+
+        if ($this->test) {
+            print_r($this->data);
+            echo PHP_EOL;
+        }
+    }
+
+    public function getResult(): array
+    {
+        return [
+            $this->getPart1(),
+            $this->getPart2()
+        ];
+    }
+
+    private function getPart1(): int
+    {
+        return 0;
+    }
+
+    private function getPart2(): int
+    {
+        return 0;
+    }
+}
